8d40ecabe54cdfe718afbd230f2c2f351d0b9fc9
missrais <miss_rais@proton.me>
2026-08-21 20:06:48 +0000

    arm64: fix Clang 15+ compatibility
    - stack_pointer.h: use inline asm instead of global register
      variable which newer Clang rejects
    - spinlock.h: change 'I' to 'r' constraint for TICKET_SHIFT
      value 65536 which exceeds 12-bit ARM immediate range

--- arch/arm64/include/asm/stack_pointer.h
+++ arch/arm64/include/asm/stack_pointer.h
@@ -2,9 +2,16 @@
 #ifndef __ASM_STACK_POINTER_H
 #define __ASM_STACK_POINTER_H
 
-/*
- * how to get the current stack pointer from C
- */
+#ifndef __clang__
 register unsigned long current_stack_pointer asm ("sp");
+#else
+static __always_inline unsigned long current_stack_pointer(void)
+{
+	unsigned long sp;
+	asm("mov %0, sp" : "=r"(sp));
+	return sp;
+}
+#define current_stack_pointer current_stack_pointer()
+#endif
 
 #endif /* __ASM_STACK_POINTER_H */
