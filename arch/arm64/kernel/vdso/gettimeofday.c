3786e6791f6b6c5c0fb272b4b4a42afe72bfbde1
missrais <miss_rais@proton.me>
2026-08-22 04:38:57 +0000

    arm64: vdso: fix inline asm register constraints and Makefile cmd_vdsoas

--- arch/arm64/kernel/vdso/gettimeofday.c
+++ arch/arm64/kernel/vdso/gettimeofday.c
@@ -1,8 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Userspace implementations of gettimeofday() and friends.
- * Pure C implementation for LLVM IAS compatibility.
- */
 #include <linux/types.h>
 #include <linux/time.h>
 #include <linux/errno.h>
@@ -10,8 +6,6 @@
 #include <asm/unistd.h>
 #include <asm/barrier.h>
 
-#define NSEC_PER_SEC		1000000000LL
-#define LOW_RES_NSEC		TICK_NSEC
 #define VDSO_CLOCK_COARSE_RES	4000000
 
 extern struct vdso_data _vdso_data;
@@ -24,7 +18,6 @@ static notrace __always_inline const struct vdso_data *get_vdso_data(void)
 static notrace __always_inline u32 vdso_read_begin(const struct vdso_data *vd)
 {
 	u32 seq;
-
 	while ((seq = READ_ONCE(vd->tb_seq_count)) & 1)
 		cpu_relax();
 	smp_rmb();
@@ -42,7 +35,6 @@ static notrace __always_inline int do_realtime(const struct vdso_data *vd,
 					       struct timespec *ts)
 {
 	u32 seq;
-
 	do {
 		seq = vdso_read_begin(vd);
 		if (READ_ONCE(vd->use_syscall))
@@ -50,7 +42,6 @@ static notrace __always_inline int do_realtime(const struct vdso_data *vd,
 		ts->tv_sec  = vd->xtime_clock_sec;
 		ts->tv_nsec = vd->xtime_clock_nsec;
 	} while (vdso_read_retry(vd, seq));
-
 	return 0;
 }
 
@@ -58,64 +49,69 @@ static notrace __always_inline int do_coarse(const struct vdso_data *vd,
 					     struct timespec *ts)
 {
 	u32 seq;
-
 	do {
 		seq = vdso_read_begin(vd);
 		ts->tv_sec  = vd->xtime_coarse_sec;
 		ts->tv_nsec = vd->xtime_coarse_nsec;
 	} while (vdso_read_retry(vd, seq));
-
 	return 0;
 }
 
-static notrace __always_inline
-long clock_gettime_fallback(clockid_t clock, struct timespec *ts)
+static notrace __always_inline long
+clock_gettime_fallback(clockid_t clock, struct timespec *ts)
 {
-	register long ret	asm("x0");
-	register clockid_t clk	asm("x0") = clock;
-	register struct timespec *t asm("x1") = ts;
-	register long nr	asm("x8") = __NR_clock_gettime;
-
-	asm volatile("svc #0" : "=r"(ret) : "r"(clk), "r"(t), "r"(nr)
-		     : "memory");
+	long ret;
+	asm volatile(
+		"mov x0, %1\n"
+		"mov x1, %2\n"
+		"mov x8, %3\n"
+		"svc #0\n"
+		"mov %0, x0\n"
+		: "=r" (ret)
+		: "r" ((long)clock), "r" (ts), "i" (__NR_clock_gettime)
+		: "x0", "x1", "x8", "memory");
 	return ret;
 }
 
-static notrace __always_inline
-long gettimeofday_fallback(struct timeval *tv, struct timezone *tz)
+static notrace __always_inline long
+gettimeofday_fallback(struct timeval *tv, struct timezone *tz)
 {
-	register long ret	asm("x0");
-	register struct timeval *tvp asm("x0") = tv;
-	register struct timezone *tzp asm("x1") = tz;
-	register long nr	asm("x8") = __NR_gettimeofday;
-
-	asm volatile("svc #0" : "=r"(ret) : "r"(tvp), "r"(tzp), "r"(nr)
-		     : "memory");
+	long ret;
+	asm volatile(
+		"mov x0, %1\n"
+		"mov x1, %2\n"
+		"mov x8, %3\n"
+		"svc #0\n"
+		"mov %0, x0\n"
+		: "=r" (ret)
+		: "r" (tv), "r" (tz), "i" (__NR_gettimeofday)
+		: "x0", "x1", "x8", "memory");
 	return ret;
 }
 
-static notrace __always_inline
-long clock_getres_fallback(clockid_t clock, struct timespec *ts)
+static notrace __always_inline long
+clock_getres_fallback(clockid_t clock_id, struct timespec *ts)
 {
-	register long ret	asm("x0");
-	register clockid_t clk	asm("x0") = clock;
-	register struct timespec *t asm("x1") = ts;
-	register long nr	asm("x8") = __NR_clock_getres;
-
-	asm volatile("svc #0" : "=r"(ret) : "r"(clk), "r"(t), "r"(nr)
-		     : "memory");
+	long ret;
+	asm volatile(
+		"mov x0, %1\n"
+		"mov x1, %2\n"
+		"mov x8, %3\n"
+		"svc #0\n"
+		"mov %0, x0\n"
+		: "=r" (ret)
+		: "r" ((long)clock_id), "r" (ts), "i" (__NR_clock_getres)
+		: "x0", "x1", "x8", "memory");
 	return ret;
 }
 
 notrace int __kernel_clock_gettime(clockid_t clock, struct timespec *ts)
 {
 	const struct vdso_data *vd = get_vdso_data();
-	int ret;
 
 	switch (clock) {
 	case CLOCK_REALTIME:
-		ret = do_realtime(vd, ts);
-		if (!ret)
+		if (!do_realtime(vd, ts))
 			return 0;
 		break;
 	case CLOCK_REALTIME_COARSE:
@@ -124,7 +120,6 @@ notrace int __kernel_clock_gettime(clockid_t clock, struct timespec *ts)
 	default:
 		break;
 	}
-
 	return clock_gettime_fallback(clock, ts);
 }
 
@@ -134,19 +129,15 @@ notrace int __kernel_gettimeofday(struct timeval *tv, struct timezone *tz)
 
 	if (likely(tv != NULL)) {
 		struct timespec ts;
-
 		if (do_realtime(vd, &ts))
 			return gettimeofday_fallback(tv, tz);
-
 		tv->tv_sec  = ts.tv_sec;
 		tv->tv_usec = ts.tv_nsec / 1000;
 	}
-
 	if (unlikely(tz != NULL)) {
 		tz->tz_minuteswest = vd->tz_minuteswest;
 		tz->tz_dsttime     = vd->tz_dsttime;
 	}
-
 	return 0;
 }
 
@@ -154,7 +145,6 @@ notrace int __kernel_clock_getres(clockid_t clock_id, struct timespec *res)
 {
 	if (res == NULL)
 		return 0;
-
 	switch (clock_id) {
 	case CLOCK_REALTIME:
 	case CLOCK_MONOTONIC:
