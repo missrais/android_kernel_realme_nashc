ac2cfe82bc73abda32bdd310c218cdff98e42efd
missrais <miss_rais@proton.me>
2026-08-22 10:47:53 +0000

    nashc: fix missing headers - upmu_hw mt6781, scp_helper, printk_ctrl conflict

--- drivers/misc/mediatek/include/mt-plat/mtk_printk_ctrl.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_printk_ctrl.h
@@ -10,7 +10,7 @@
 #include <linux/sched.h>
 
 #ifdef CONFIG_MTK_PRINTK_UART_CONSOLE
-bool mt_get_uartlog_status(void);
+// removed conflicting declaration
 void mt_disable_uart(void);
 void mt_enable_uart(void);
 #else
